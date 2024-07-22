7d93be3b427d7c2facfb44cb16639423464763b8
Gokul Modi <gokul@itoneclick.com>
2024-07-22 11:44:14 +0530

    used useValue provider for mockServices

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,12 +2,31 @@ import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
 
+const mockSongService = {
+  findAll() {
+    return [
+      {
+        id: 1,
+        title: 'Song 1',
+        artist: 'Artist 1',
+        genre: 'Genre 1',
+      },
+    ];
+  },
+};
+
 @Module({
   controllers: [SongsController],
   providers: [
+    //standard provider
+    // {
+    //   provide: SongsService,
+    //   useClass: SongsService,
+    // },
+    //mock provider
     {
       provide: SongsService,
-      useClass: SongsService,
+      useValue: mockSongService,
     },
   ],
 })
