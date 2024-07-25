bdfbfb4500f1f9341ae6a4cf60c16e1d48945669
Gokul Modi <gokul@itoneclick.com>
2024-07-25 12:11:11 +0530

    disable the data seeding

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SeedService } from './seed/seed.service';
+// import { SeedService } from './seed/seed.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const seedService = app.get(SeedService);
-  await seedService.seed();
+  /**
+   * Enable the database seeding from here if needed
+   * just uncomment the commented lines
+   */
+  // const seedService = app.get(SeedService);
+  // await seedService.seed();
   await app.listen(3000);
 }
 bootstrap();
