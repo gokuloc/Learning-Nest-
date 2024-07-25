c235d11206d68f6947da2dc30018c4a7b719809c
Gokul Modi <gokul@itoneclick.com>
2024-07-25 17:39:39 +0530

    update the swagger api route

--- src/main.ts
+++ src/main.ts
@@ -37,8 +37,9 @@ async function bootstrap() {
     )
     .build();
 
+  // http://localhost:3000/api/documentation
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/documentation', app, document);
 
   await app.listen(configService.get<number>('port'));
 }
